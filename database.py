b46af272b8d37633a20a9f6b62bff5442e9d5194
RohanDeveloper80 <argupta1010101@gmail.com>
2026-06-19 02:33:49 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,22 +1,7 @@
-import sqlite3
-
-def create_database():
-    conn = sqlite3.connect("bot.db")
-
-    cursor = conn.cursor()
-
-    cursor.execute("""
-    CREATE TABLE IF NOT EXISTS orders (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        order_id TEXT,
-        user_id TEXT,
-        amount INTEGER,
-        utr TEXT,
-        status TEXT
-    )
-    """)
-
-    conn.commit()
-    conn.close()
-
-    print("Database Ready")
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS inventory (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    account_data TEXT,
+    status TEXT DEFAULT 'available'
+)
+""")

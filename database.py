4e7344e8f50282bb8241d8531723b78e9012f91b
RohanDeveloper80 <argupta1010101@gmail.com>
2026-06-19 02:29:36 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,2 +1,22 @@
-def hello_database():
-    return "Database module loaded successfully"
+import sqlite3
+
+def create_database():
+    conn = sqlite3.connect("bot.db")
+
+    cursor = conn.cursor()
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS orders (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        order_id TEXT,
+        user_id TEXT,
+        amount INTEGER,
+        utr TEXT,
+        status TEXT
+    )
+    """)
+
+    conn.commit()
+    conn.close()
+
+    print("Database Ready")

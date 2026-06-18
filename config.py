5939d06770a484cb37eaf3dd29a1a1e0a92992c9
RohanDeveloper80 <argupta1010101@gmail.com>
2026-06-19 00:16:44 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 import os
 
-TOKEN = os.getenv("8978417488:AAFgZQVp1CtBhi4FrT4RdTGbpjp-s-lUf9A")
-ADMIN_ID = int(os.getenv("5818126906"))
-UPI_ID = os.getenv("eliteascent@naviaxis")
+TOKEN = os.getenv("TOKEN")
+ADMIN_ID = int(os.getenv("ADMIN_ID"))
+UPI_ID = os.getenv("UPI_ID")
+
 PRICE_PER_ACCOUNT = 20
